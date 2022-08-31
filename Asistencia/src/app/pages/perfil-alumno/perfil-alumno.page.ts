94116404f5b50e3d7576ef0c56fec8a63a81ffb5
YzanBaezz <yz.baez@duocuc.cl>
2022-08-31 18:45:12 -0400

    avance1.2

--- Asistencia/src/app/pages/perfil-alumno/perfil-alumno.page.ts
+++ Asistencia/src/app/pages/perfil-alumno/perfil-alumno.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationExtras } from '@angular/router';
 @Component({
   selector: 'app-perfil-alumno',
   templateUrl: './perfil-alumno.page.html',
   styleUrls: ['./perfil-alumno.page.scss'],
 })
 export class PerfilAlumnoPage implements OnInit {
-
-  constructor() { }
+subjects;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.subjects = [
+      {
+        img: 'assets/image',
+        name: 'English'
+      },
+      {
+        img: 'assets/image',
+        name: 'English'
+      },
+      {
+        img: 'assets/image',
+        name: 'English'
+      },
+      {
+        img: 'assets/image',
+        name: 'English'
+      },
+    ]
   }
 
+  goToSubject(){
+
+  }
 }
--- Asistencia/src/app/pages/perfil-profesor/perfil-profesor.page.ts
+++ Asistencia/src/app/pages/perfil-profesor/perfil-profesor.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationExtras } from '@angular/router';
 @Component({
   selector: 'app-perfil-profesor',
   templateUrl: './perfil-profesor.page.html',
   styleUrls: ['./perfil-profesor.page.scss'],
 })
 export class PerfilProfesorPage implements OnInit {
-
-  constructor() { }
+subjects;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.subjects = [
+      {
+        img: 'assets/image',
+        name: 'English'
+      },
+      {
+        img: 'assets/image',
+        name: 'English'
+      },
+      {
+        img: 'assets/image',
+        name: 'English'
+      },
+      {
+        img: 'assets/image',
+        name: 'English'
+      },
+    ]
   }
 
 }
