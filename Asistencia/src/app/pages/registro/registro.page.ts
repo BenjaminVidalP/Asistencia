a387dc31dc94916a9c461d5ded07ce021226b921
YzanBaezz <yz.baez@duocuc.cl>
2022-09-01 23:27:25 -0400

    cambios 3.0

--- Asistencia/src/app/pages/registro/registro.page.ts
+++ Asistencia/src/app/pages/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MenuController } from '@ionic/angular';
 
 @Component({
@@ -14,6 +15,17 @@ export class RegistroPage implements OnInit {
     password:''
 
   };
+  emailPattern: any = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  createFormGroup(){
+    return new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.minLength(5),
+      Validators.pattern(this.emailPattern)]),
+      name: new FormControl('', [Validators.required, Validators.minLength(4),
+        Validators.maxLength(16)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), 
+        Validators.maxLength(10)]),
+    })
+  }
 
   constructor(private menu: MenuController) { 
     this.menu.enable(false);
